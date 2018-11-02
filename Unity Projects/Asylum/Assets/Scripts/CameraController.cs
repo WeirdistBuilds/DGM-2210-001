1c28fd6a98a468a1a3fdc64184a65ca665fddc53
Kyle Hinckley <kylehinckley@hotmail.com>
2018-11-01 21:42:05 -0600

    Fixed Animation, started character controller, layout

---
+++ Unity Projects/Asylum/Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+	public float minX = -60f, maxX = 60f, minY = -360f, maxY = 360f;
+	public float sensX = 15f, sensY = 15f;
+	public Camera cam;
+
+	private float rotY = 0f, rotX = 0f;
+	
+	
+	void Start ()
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+
+		if (Input.GetKey(KeyCode.Escape))
+		{
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+		}
+	}
+	
+	void Update ()
+	{
+		rotY += Input.GetAxis("Mouse X") * sensY;
+		rotX += Input.GetAxis("Mouse Y") * sensX;
+		rotX = Mathf.Clamp(rotX, minX, maxX);
+		
+		transform.localEulerAngles = new Vector3(0, rotY, 0);
+		cam.transform.localEulerAngles = new Vector3(-rotX, rotY, 0);
+	}
+}
---
+++ Unity Projects/Asylum/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+	public float Speed;
+	private Rigidbody rigidbody;
+	private Vector3 moveDirection;
+	private Vector3 gravFix;
+	private float hMove;
+	private float vMove;
+
+	void Start()
+	{
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	
+	void Update()
+	{
+		hMove = Input.GetAxis("Horizontal");
+		vMove = Input.GetAxis("Vertical");
+
+		moveDirection = (hMove * transform.right + vMove * transform.forward).normalized;
+		
+		gravFix = new Vector3(0, rigidbody.velocity.y, 0);
+		rigidbody.velocity = moveDirection * Speed * Time.deltaTime;
+		rigidbody.velocity += gravFix;
+	}
+}
