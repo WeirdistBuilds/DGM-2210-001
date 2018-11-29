a9202824197e6d8343d34d099a3c6d501ac2024f
Kyle Hinckley <kylehinckley@hotmail.com>
2018-11-29 01:29:25 -0700

    SeaEnvironment in Unity, added projector

---
+++ Unity Projects/SeaEnvironment/Assets/Scripts/AnimatedProjector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedProjector : MonoBehaviour {
+
+    public float fps = 30.0f;
+    public Texture2D[] frames;
+
+    private int frameIndex;
+    private Projector projector;
+
+    private void Start()
+    {
+        projector = GetComponent<Projector>();
+        NextFrame();
+        InvokeRepeating("NextFrame", 1 / fps, 1 / fps);
+    }
+
+    void NextFrame()
+    {
+        projector.material.SetTexture("_ShadowTex", frames[frameIndex]);
+        if (frameIndex >= frames.Length - 1)
+        {
+            frameIndex = 0;
+        }
+        else
+        {
+            frameIndex++;
+            Debug.Log(frameIndex.ToString());
+        }
+    }
+}
